590d0efccef0d4adbb5aa6945c4e750b418b590c
Vachagan <vachagan.grigoryan@outlook.com>
2023-05-18 00:15:16 +0400

    fixed calculator

--- calculator.py
+++ calculator.py
@@ -12,7 +12,7 @@ def calculate(expression):
         if item.isdecimal() or item.lstrip('-').replace('.', '', 1).isdecimal():
             operations_stack.append(item)
         elif item in operators and len(operations_stack) > 1:
-            result = eval(f'{operations_stack.pop()}{item}{operations_stack.pop()}')
+            result = eval(f'{operations_stack.pop()}{operators.get(item)}{operations_stack.pop()}')
             operations_stack.append(result)
         else:
             raise ValueError('Invalid expression')
@@ -27,4 +27,4 @@ print(calculate('+ 2 3'))  # 5
 print(calculate('* 6 + 4 5'))  # 54
 print(calculate('+ - / 7 5 6 - 4 6'))  # 54
 
-print(calculate("+ - * 2 3 3 / / 8 4 + 1 1"))  # 2×3−3+8/4/(1+1) = 4.0
+print(calculate("+ - * 2 3 3 / / 8 4 add 1 1"))  # 2×3−3+8/4/(1+1) = 4.0
